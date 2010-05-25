19856458f227b60f4ba7a99110c44cf832e077ac
blackyaime <blackyaime@7e77a2e6-e081-f6ba-deb7-35bd0c21aff9>
2010-05-25 19:11:52 +0000

    another test

---
+++ trunk/main.c
@@ -0,0 +1,14 @@
+/*
+** main.c for tes in /u/all/iniong_a/cu/zappy
+** 
+** Made by aime-bijou iniongo
+** Login   <iniong_a@epitech.net>
+** 
+** Started on  Tue May 25 21:08:43 2010 aime-bijou iniongo
+** Last update Tue May 25 21:09:00 2010 aime-bijou iniongo
+*/
+
+int	main()
+{
+  return (0);
+}
