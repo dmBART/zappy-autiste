6af4ec2fb865f3631955c3300b6c508e62f74c40
blackyaime <blackyaime@7e77a2e6-e081-f6ba-deb7-35bd0c21aff9>
2010-05-25 17:51:24 +0000

    juste un test

---
+++ trunk/src/main.c
@@ -0,0 +1,14 @@
+/*
+** main.c for test in /u/all/iniong_a/cu/test/graphic
+** 
+** Made by aime-bijou iniongo
+** Login   <iniong_a@epitech.net>
+** 
+** Started on  Tue May 25 19:45:23 2010 aime-bijou iniongo
+** Last update Tue May 25 19:45:59 2010 aime-bijou iniongo
+*/
+
+int	main(void)
+{
+  return (0);
+}
