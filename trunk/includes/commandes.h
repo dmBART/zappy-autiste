1b88df0ce000903602ba9317df90160f6e28744d
blackyaime <blackyaime@7e77a2e6-e081-f6ba-deb7-35bd0c21aff9>
2010-06-11 14:03:59 +0000

    derniere version fontionnelle : gestion de du temps + commandes de milbau : a ce soir peut etre pour une version de release

---
+++ trunk/includes/commandes.h
@@ -0,0 +1,37 @@
+/*
+** commandes.h for zappy in /u/all/milbau_a/cu/zappy
+** 
+** Made by alexis milbault
+** Login   <milbau_a@epitech.net>
+** 
+** Started on  Thu Jun  3 16:27:34 2010 alexis milbault
+** Last update Thu Jun 10 19:06:18 2010 alexis milbault
+*/
+#ifndef _COMMANDES_H_
+#define _COMMANDES_H_
+
+#include "../includes/server.h"
+
+typedef struct	s_cmd
+{
+  char		*cmd;
+  void		(*f)(t_desc *, t_play *, char **);
+}		t_cmd;
+
+t_cmd	cmd_tab[]= {
+  {"avance", move_forward},
+  {"droite", turn_right},
+  {"gauche", turn_left},
+  {"voir", see},
+  {"inventaire", inventory},
+  {"prend", take_object},
+  {"pose", drop_object},
+  /*  {"expulse", },
+  {"broadcast", },
+  {"fork", },
+  {"connect_nbr", },*/
+  {NULL, NULL},
+};
+
+
+#endif /*_COMMANDES_H_*/
