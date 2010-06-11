464c60df9b9bc6baf63d5387b7e2dfc4c2829677
blackyaime <blackyaime@7e77a2e6-e081-f6ba-deb7-35bd0c21aff9>
2010-06-11 01:35:52 +0000

    gestion timer complete normalement + il manque juste un point .h de mibau que je rajouterais demain :p

--- trunk/src_server/client_management.c
+++ trunk/src_server/client_management.c
@@ -5,7 +5,7 @@
 ** Login   <iniong_a@epitech.net>
 ** 
 ** Started on  Fri May 28 00:49:11 2010 aime-bijou iniongo
-** Last update Thu Jun 10 04:05:17 2010 aime-bijou iniongo
+** Last update Fri Jun 11 03:22:13 2010 aime-bijou iniongo
 */
 
 #include <sys/socket.h>
@@ -18,10 +18,6 @@
 void		my_putchar(char c)
 {
   write(1, &c, 1);
-}
-
-int		cmp_time()
-{
 }
 
 void		close_client(t_play *player, t_env *e)
@@ -101,37 +97,34 @@ void		manage_client(t_desc *serv, t_play *players, t_env *e, t_timev t)
 
   e->i = -1;
   while (e->i++ < MAX_FD)
-    if (players[e->i].type == FD_CLIENT && FD_ISSET(players[e->i].cs, &e->readfs))
-      {
-	memset(buff, 0, 4090);
-	n = xread(players[e->i].cs, buff, 4090);
-	if (n == 0)
-	  close_client(players, e);
-	else
-	  {
-	    manage_buff(&players[e->i], buff, n);
-	    client_write(serv, players, e, t, n);
-	  }
-      }
-  gettimeofday(&e->tv, &e->ts);
-  x = -1;
-  while(x++ < MAX_FD)
-      if (players[x].cs == t.cs)
-	break;
-  if (e->tv.tv_sec >= t.t_old.tv_sec &&  e->tv.tv_usec >= t.t_old.tv_usec)
     {
-      if (players[x].type == FD_CLIENT)
+      if (players[e->i].type == FD_CLIENT && FD_ISSET(players[e->i].cs, &e->readfs))
 	{
-	  write(t.cs, "Ok\n", 3);
-	  printf("time of execution = %ld\n", t.t);
- 	  del_elem_to_queu(&serv->t, t);
+	  memset(buff, 0, 4090);
+	  n = xread(players[e->i].cs, buff, 4090);
+	  if (n == 0)
+	    close_client(players, e);
+	  else
+	    {
+	      manage_buff(&players[e->i], buff, n);
+	      client_write(serv, players, e, t, n);
+	    }
 	}
+      gettimeofday(&e->tv, &e->ts);
+      if (e->tv.tv_sec >= t.t_old.tv_sec &&  e->tv.tv_usec > t.t_old.tv_usec
+	  && serv->tv != NULL && t.d == 1)
+	{
+	  if (players[e->i].type == FD_CLIENT)
+	    {
+	      t.d = 0;
+	      /* 	  write(t.cs, "Ok\n", 3);
+			  printf("time of execution = %ld\n", t.t);*/
+	      del_elem_to_queu(&serv->tv, t);
+	    }
+	}
+      else
+	update_time_struct(serv->tv, e);
     }
-  else
-    {
-      printf("execution dans ... %d\n", t.t);
-      update_time_struct(serv->tv, e);
-      }
 }
 
 void			add_players(int s, t_env *e, t_play *players)
